ffcf56f83ebf3ef66db8707b304015a9413e552f
saiful-azree <azreeayie24@gmail.com>
2023-03-31 09:53:27 +0800

    function login exercise

--- index.js
+++ index.js
@@ -29,8 +29,35 @@ function login(username, password){
     
 }
 
+function register(newusername, newpassword, newname, newemail){
+    //TODO: Check if username exist
+    let checked = dbUsers.find(element => element.username == newusername)
+    if(checked){
+        return "Username has been taken"
+    }
+    else{
+        dbUsers.push({
+            username: newusername,
+            password: newpassword,
+            name: newname,
+            email: newemail
+        })
+    }
+}
+
+
 //try to login (TRUE)
 //console.log( login("saiful", "012345"))
 
-//try to login (FALSE)
-console.log( login("ainur", "111111"))
+//try to login (FALSE password)
+//console.log( login("saiful", "111111"))
+
+//try to login (FALSE username)
+//console.log( login("sepul", "012345"))
+
+//try to register (TRUE)
+//register( "ainur", "111111", "Ainur Syamira", "ainur@gmail.com"))
+//console.log( login("ainur", "111111"))
+
+//try to register (FALSE)
+console.log(register( "saiful", "abcdef", "Sepul Eerza", "eerza@gmail.com"))
